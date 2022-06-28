d4d03b7fcd4205bfb6135733bfd195c820afc903
Tien Pham Van <tienpv14701@gmail.com>
2022-06-28 17:17:43 +0700

    Optimize selenium load and add element query option (id, class)

--- src/main/groovy/controller/LANDINGPAGE_CRAWLER.groovy
+++ src/main/groovy/controller/LANDINGPAGE_CRAWLER.groovy
@@ -1,10 +1,13 @@
 package controller
 
 import app.AppConfig
+import com.github.benmanes.caffeine.cache.Caffeine
+import com.github.benmanes.caffeine.cache.LoadingCache
 import groovy.transform.InheritConstructors
 import io.vertx.core.http.HttpServerRequest
 import io.vertx.core.http.HttpServerResponse
 import io.vertx.ext.web.RoutingContext
+import org.apache.commons.lang3.StringUtils
 import org.apache.commons.lang3.Validate
 import org.openqa.selenium.PageLoadStrategy
 import org.openqa.selenium.WebElement
@@ -14,27 +17,46 @@ import org.openqa.selenium.remote.RemoteWebDriver
 import util.Constants
 import vertx.VertxController
 
+import java.util.concurrent.TimeUnit
+
 @InheritConstructors
 class LANDINGPAGE_CRAWLER extends VertxController<AppConfig> {
+    LoadingCache<List<String>, String> htmlContentCache = Caffeine.newBuilder()
+            .expireAfterWrite(5, TimeUnit.MINUTES)
+            // Ex: ["https://beacons.ai/covi", "root", "id"] -> <div>HTML content</div>
+            .build { queryParams ->
+                crawlHtmlContent(queryParams[0], queryParams[1], queryParams[2])
+            }
+
     @Override
     void validate(RoutingContext context, HttpServerRequest request) {
         String url = context.request().getParam("url") ?: ""
-        String id = context.request().getParam("id") ?: "root"
+        String element = context.request().getParam("element") ?: ""
+        String queryOption = context.request().getParam("query_option") ?: ""
 
         Validate.isTrue(url ==~ Constants.URL_REGEX, "Input URL is not valid")
+        Validate.isTrue(!StringUtils.isEmpty(element), "Input element is not valid")
+        Validate.isTrue(!StringUtils.isEmpty(queryOption), "Input query_option is not valid")
 
-        context.put("id", id)
         context.put("url", url)
+        context.put("element", element)
+        context.put("query_option", queryOption)
     }
 
     @Override
     void handle(RoutingContext context, HttpServerRequest request, HttpServerResponse response) {
-        String id = context.get("id")
         String url = context.get("url")
+        String element = context.get("element")
+        String queryOption = context.get("query_option")
 
+        String crawledHtml = htmlContentCache.get(["$url", "$element", "$queryOption"])
+        writeSuccessResponse(context.response(), crawledHtml)
+    }
+
+    String crawlHtmlContent(String url, String element, String queryOption) {
         ChromeOptions options = new ChromeOptions()
                 .setHeadless(true)
-                .setPageLoadStrategy(PageLoadStrategy.NORMAL)
+                .setPageLoadStrategy(PageLoadStrategy.EAGER)
                 .addArguments("–-disable-extensions", "--mute-audio")
                 .addArguments("--silent")
                 .addArguments("--start-maximized")
@@ -48,15 +70,23 @@ class LANDINGPAGE_CRAWLER extends VertxController<AppConfig> {
         RemoteWebDriver browserDriver
         try {
             browserDriver = new ChromeDriver(options)
+            browserDriver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES)
+
             browserDriver.get(url)
 
-            WebElement rootElement = browserDriver.findElementById(id)
+            WebElement rootElement
+            if (StringUtils.equals(queryOption, "id")) {
+                rootElement = browserDriver.findElementById(element)
+            } else if (StringUtils.equals(queryOption, "class")) {
+                rootElement = browserDriver.findElementByClassName(element)
+            } else {
+                return ""
+            }
             String crawledHtml = rootElement.getAttribute('innerHTML') ?: ""
-
-            writeSuccessResponse(context.response(), crawledHtml)
+            return crawledHtml
         } catch (e) {
-            writeSuccessResponse(context.response(), e.message)
-            throw new RuntimeException(e.message)
+            e.printStackTrace()
+            return ""
         } finally {
             browserDriver?.close()
             browserDriver?.quit()
